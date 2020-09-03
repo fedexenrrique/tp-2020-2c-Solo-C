32465b1e55001596f6ec20f26a39c479f9143bdf
Fede Enrrique <fede.enrrique@gmail.com>
2020-09-03 14:25:59 -0300

    agrego archivos de configuración deafult del TP

--- app/src/app.c
+++ app/src/app.c
@@ -1,17 +1,12 @@
-/*
- ============================================================================
- Name        : app.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
 #include "app.h"
 
 int main(void) {
-	puts("!!!Hello World!!!");
+
 	prueba_biblioteca_compartida();
+
+
+
+
 	return EXIT_SUCCESS;
 }
