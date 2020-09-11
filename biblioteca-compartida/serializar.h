32e8f38393580300dc6942803e8e9c9291fecd4f
Fede Enrrique <fede.enrrique@gmail.com>
2020-09-11 16:23:14 -0300

    funciones auxiliares de cliente...

--- biblioteca-compartida/serializar.h
+++ biblioteca-compartida/serializar.h
@@ -113,10 +113,11 @@ void     prueba_biblioteca_compartida   (void                   );
 
 int      crear_socket_y_conectar        (char* ip, int puerto   );
 
-int  crear_socket_escucha           ( char * p_ip, char * p_puerto );
-int  aceptar_conexion               ( int p_socket_para_escuchar   );
-int      recibir_confirmacion           ( int   socket_cliente  );
-int      detectar_comando               ( char * p_comando      );
+int     crear_socket_escucha           ( char * p_ip, char * p_puerto );
+int     aceptar_conexion               ( int p_socket_para_escuchar   );
+int     recibir_confirmacion           ( int   socket_cliente  );
+int     detectar_comando               ( char * p_comando      );
+char *  nro_comando_a_texto(int p_comando);
 
 bool       enviar_buffer            ( int p_conexion, t_header * p_header );
 t_header * recibir_buffer           ( int socket_cliente );
